ed033e1623ee07034ba4e9d6e654dd3b8da44888
nRayen <rayen.ndiaye@gmail.com>
2024-03-08 23:28:47 +0100

    Changement de couleur et historique des couleurs ajoutés

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-let cases = document.querySelectorAll('td')
+let cases = document.querySelectorAll('.box')
+let selector = document.querySelector('#color-input')
+let last = document.querySelectorAll('.last-color')
+let color = selector.value
+let history = []
 
-for (let c of cases) {
-    c.addEventListener("onclick", (e) => {
-        console.log(e.currentTarget)
-    }, false)
-}
+// Changer la couleur sélectionnée
+selector.addEventListener('change', () => {
+    color = selector.value
+    history.push(color)
 
-cases.forEach(c => {
-    c.addEventListener('click', () => {
-        c.style.backgroundColor = "red";
-    }, true)
+    for(let i=0; i<5; i++) {
+        last[i].style.backgroundColor = history[history.length-i-1];
+        last[i].dataset.color = history[history.length-i-1]
+
+        history[history.length-i-1] != null ? last[i].style.visibility = 'visible' : 'hidden'
+    }
 })
 
+// Changer couleur des cases
+cases.forEach(c => {
+    c.addEventListener('click', () => {
+        c.style.backgroundColor = color;
+    })
+})
 
+// Historique couleurs
+last.forEach(btn => {
+    btn.addEventListener('click', () => {
+        selector.value = btn.dataset.color
+        color = btn.dataset.color
+        history.push(color)
+    })
+})
